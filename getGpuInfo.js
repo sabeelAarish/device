a88c629abe4e9cb04a22f9c45dce30b2be7af595
sabeelAarish <81349433+sabeelAarish@users.noreply.github.com>
2021-06-30 15:01:15 +0530

    Update getGpuInfo.js

--- getGpuInfo.js
+++ getGpuInfo.js
@@ -14,7 +14,7 @@ document.getElementById("renderer").innerHTML = "Renderer Info:&nbsp&nbsp" + "" 
 }
 
 function getUserAgent() {
-    var x = navigator.userAgent.match(/;\s*([^;]+)\s+Build\//g);
+    var x = navigator.userAgent.match(/[^;\s][^;]+?(?=\s+build\b)/i);
     document.getElementById("userAgent").innerHTML ="User Agent Info:&nbsp&nbsp " + x;
     var y = navigator.deviceMemory;
     document.getElementById("deviceMemory").innerHTML ="Device Memory:&nbsp&nbsp " + y + "GB";
